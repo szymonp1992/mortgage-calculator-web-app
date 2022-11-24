25e937d87d483fc68babf1aadd3aefd14c17ca5d
Szymon Podstufka (Prywatne) <szymonpodstufka@MBP-Szymon.kopnet.pl>
2022-11-24 21:54:59 +0100

    Added all the logic except from calculating the monthly payment

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,172 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="app-container">
+    <h2>Mortgage calculator</h2>
+    <div class="calculator-container">
+      <div>
+        <label for="purchase-price">Purchase price: ${{ purchasePrice }}</label>
+        <input
+          type="range"
+          id="purchase-price"
+          name="purchase-price"
+          min="0"
+          max="1000000"
+          step="10000"
+          :value="purchasePrice"
+          @input="changeValue"
+        />
+      </div>
+      <div>
+        <label for="down-payment">Down payment: ${{ downPayment }}</label>
+        <input
+          type="range"
+          id="down-payment"
+          name="down-payment"
+          min="0"
+          max="1000000"
+          step="10000"
+          :value="downPayment"
+          @input="changeValue"
+        />
+      </div>
+      <div>
+        <label for="repayment-time"
+          >Repayment time: {{ repaymentTime }} years</label
+        >
+        <input
+          type="range"
+          id="repayment-time"
+          name="repayment-time"
+          min="1"
+          max="25"
+          step="1"
+          :value="repaymentTime"
+          @input="changeValue"
+        />
+      </div>
+      <div>
+        <label for="interest-rate"
+          >Interest rate: {{ (interestRate * 100).toFixed() }}%</label
+        >
+        <input
+          type="range"
+          id="interest-rate"
+          name="interest-rate"
+          min="0"
+          max="0.2"
+          step="0.01"
+          :value="interestRate"
+          @input="changeValue"
+        />
+      </div>
+      <div>
+        <p>Loan amount</p>
+        <h2>${{ loanAmount }}</h2>
+      </div>
+      <div>
+        <p>Estimated pr. month:</p>
+        <h2>${{ perMonth }}</h2>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const purchasePrice = ref(500000);
+    const downPayment = ref(100000);
+    const repaymentTime = ref(5);
+    const interestRate = ref(0.03);
+    const loanAmount = ref(0);
+    const perMonth = ref(0);
+
+    onMounted(() => {
+      loanAmount.value = purchasePrice.value - downPayment.value;
+    });
+
+    function calculateValue(p, i, n) {
+      return ((p * (((i * (1 + i)) ^ n) / ((1 + i) ^ (n - 1)))) / n)
+    }
+
+    function changeValue(e) {
+      if (e.target.id === "purchase-price") {
+        purchasePrice.value = e.target.value;
+      }
+      if (e.target.id === "down-payment") {
+        downPayment.value = e.target.value;
+      }
+      if (e.target.id === "repayment-time") {
+        repaymentTime.value = e.target.value;
+      }
+      if (e.target.id === "interest-rate") {
+        interestRate.value = e.target.value;
+      }
+      loanAmount.value = purchasePrice.value - downPayment.value;
+
+      const p = loanAmount.value;
+      const i = interestRate.value / 12;
+      const n = repaymentTime.value * 12;
+      perMonth.value = calculateValue(p, i, n)
+    }
+
+    return {
+      purchasePrice,
+      downPayment,
+      repaymentTime,
+      interestRate,
+      loanAmount,
+      perMonth,
+      changeValue,
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  background-color: #094b6e;
+  color: white;
+  font-family: Roboto, Arial, sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.app-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 60%;
+  width: 80%;
+  border: 10px solid white;
+  border-radius: 15px;
+  padding: 15px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+@media (max-width: 700px) {
+  * {
+    font-size: 0.8rem;
   }
+  .app-container {
+    height: 40%;
+  }
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.calculator-container {
+  margin: 2rem auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.calculator-container div {
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem 0;
+}
+.calculator-container div * {
+  margin: 1rem 0;
 }
 </style>
