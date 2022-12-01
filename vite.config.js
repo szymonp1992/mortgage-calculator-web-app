5794068fc0353e4dc04e8b7271e84878fd9b11db
Szymon Podstufka (Prywatne) <szymonpodstufka@MBP-Szymon.kopnet.pl>
2022-12-01 20:58:47 +0100

    Added dist

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'deploy-mortgage-calculator',
   plugins: [vue()],
   resolve: {
     alias: {
